3b0566faa7df46721a828cd6a71677b4a23240df
Ana <ana2004bogdan@gmail.com>
2025-05-14 21:03:48 +0300

    Backend fix

--- Backend/app/database.py
+++ Backend/app/database.py
@@ -2,9 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:YPtvQmUrDpwPgcTsRdqCzRIJiIZiYUYA@yamabiko.proxy.rlwy.net:19581/railway"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+DATABASE_URL = "postgresql://postgres:YPtvQmUrDpwPgcTsRdqCzRIJiIZiYUYA@yamabiko.proxy.rlwy.net:19581/railway"
+
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
